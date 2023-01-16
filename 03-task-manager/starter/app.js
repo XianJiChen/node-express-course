d74c803dda22ca36c0687cc856469215972349b1
JimmyXianJiChen <jimmyxjc@gapp.nthu.edu.tw>
2023-01-16 15:39:00 -0600

    set up the router and controller

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -2,11 +2,18 @@
 
 const express = require('express');
 const app = express();
+const tasks = require('./routes/tasks')
+
+//middleware
+app.use(express.json()) // if we don't use the we won't have that data in req.body
 
 //routes
 app.get('/hello', (req, res)=>{
     res.send('Task Manager App')
 })
+
+app.use('/api/v1/tasks', tasks)
+
 
 const port = 3000
 
