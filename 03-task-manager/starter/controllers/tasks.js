87c49b3776f08c3646bc380076410bf89385f45f
JimmyXianJiChen <jimmyxjc@gapp.nthu.edu.tw>
2023-01-18 09:54:29 -0600

    implement put vs patch function

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -4,7 +4,10 @@ const getAllTasks = async (req, res) => {
     // res.send('all items from the file')
     try {
         const tasks = await Task.find({})
-        res.status(200).json({ tasks: tasks })
+        //res.status(200).json({ tasks, amount: tasks.length })
+        res
+            .status(200)
+            .json({ success: true, data: {tasks, nbHits: tasks.length }})
     } catch (error) {
         res.status(500).json({msg: error})
     }
@@ -71,10 +74,28 @@ const deleteTask = async (req, res) => {
     }
 }
 
+const editTask = async(req, res) => {
+    try {
+        const {id:taskID} = req.params;
+        const task = await Task.findOneAndDelete({ _id: taskID }, req.body, {
+            new: true,
+            runValidators: true,
+            overwrite: true
+        })
+        if(!task){
+            return res.status(404).json({msg:`No task with id : ${taskID}`})
+        }
+        res.status(200).send({ task:null, status:'success'})
+    } catch (error) {
+        res.status(500).json({msg: error})
+    }
+}
+
 module.exports = {
     getAllTasks,
     createTask,
     getTask,
     updateTask,
-    deleteTask
+    deleteTask,
+    editTask
 }
--- 03-task-manager/starter/routes/tasks.js
+++ 03-task-manager/starter/routes/tasks.js
@@ -7,11 +7,12 @@ const {
     createTask, 
     getTask, 
     updateTask, 
-    deleteTask
+    deleteTask,
+    editTask
 } = require('../controllers/tasks')
 
 router.route('/').get(getAllTasks).post(createTask)
-router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask)
+router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask).put(editTask)
 
 
 module.exports = router
